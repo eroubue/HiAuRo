d21869a181ea380df2cdde2dca29d09fae2d0a32
嗨呀www <991876906@qq.com>
2026-05-16 17:06:10 +0800

    feat: add Data.FactState property exposing fact axis runtime state

--- HiAuRo/Data/Data.cs
+++ HiAuRo/Data/Data.cs
@@ -1,3 +1,5 @@
+using HiAuRo.FactAxis;
+
 namespace HiAuRo.Data;
 
 /// <summary>
@@ -8,4 +10,8 @@ public static class Data
     public static bool IsReady =>
         OmenTools.OmenService.GameState.IsLoggedIn &&
         OmenTools.OmenService.GameState.IsTerritoryLoaded;
+
+    /// <summary>事实轴当前运行时状态快照。FactAxis 未运行时返回 null。</summary>
+    public static FactAxis.FactState? FactState =>
+        FactTimeline.Instance is { IsRunning: true } ? FactTimeline.Instance.State : null;
 }
